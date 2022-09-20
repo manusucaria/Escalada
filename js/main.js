9f11303677ae6ad745d87291e9f2951a91dc8840
Manuel Sucaria <manusucaria@gmail.com>
2022-09-20 15:12:32 -0300

    20 del 9 1

--- js/main.js
+++ js/main.js
@@ -102,103 +102,398 @@ Stock.push(twentyfourPackPaleAle);
 Stock.push(twentyfourPackDoblePale);
 Stock.push(twentyfourPackHibiscus);
 Stock.push(twentyfourPackFaramir);
+
 /*DOM*/
+
 /*TIENDA*/
+
 /*LATA BLONDE*/
 const nombreBlonde = document.getElementById("nombreBlonde");
 nombreBlonde.innerText = `${lataBlonde.tipo} ${lataBlonde.variedad}`;
 const precioBlonde = document.getElementById("precioBlonde");
 precioBlonde.innerText = `$${lataBlonde.precio}`;
+/*SUMADOR DE CANTIDAD*/
+let total1 = document.getElementById('cantidad1');
+function sumar1 () {
+    let valorTotal1 = parseInt(total1.innerHTML);
+    total1.innerHTML = valorTotal1 + 1;
+}
+function restar1 () {
+    let valorTotal1 = parseInt(total1.innerHTML);
+    if (valorTotal1 == 1) return;
+    total1.innerHTML = valorTotal1 - 1;
+}
+document.getElementById('sumar1').addEventListener('click', sumar1);
+document.getElementById('restar1').addEventListener('click', restar1);
+function agregarAlCarrito1 () {
+    let pedidoLata1 = new Pedido(lataBlonde.tipo, lataBlonde.variedad, total1.innerHTML, lataBlonde.precio * total1.innerHTML);
+    Carrito.push(pedidoLata1);
+    const buscarBlonde = Carrito.find(producto => producto.variedad === "Blonde");
+    lataBlonde.cantidad = lataBlonde.cantidad - buscarBlonde.cantidad;
+}
+document.getElementById('botonBlonde').addEventListener('click', agregarAlCarrito1);
+console.log(Carrito);
+
 /*LATA HONEY*/
 const nombreHoney = document.getElementById("nombreHoney");
 nombreHoney.innerText = `${lataHoney.tipo} ${lataHoney.variedad}`;
 const precioHoney = document.getElementById("precioHoney");
 precioHoney.innerText = `$${lataHoney.precio}`;
+const botonHoney = document.getElementById("botonHoney");
+/*SUMADOR DE CANTIDAD*/
+let total2 = document.getElementById('cantidad2');
+function sumar2 () {
+    let valorTotal2 = parseInt(total2.innerHTML);
+    total2.innerHTML = valorTotal2 + 1;
+}
+function restar2 () {
+    let valorTotal2 = parseInt(total2.innerHTML);
+    if (valorTotal2 == 1) return;
+    total2.innerHTML = valorTotal2 - 1;
+}
+document.getElementById('sumar2').addEventListener('click', sumar2);
+document.getElementById('restar2').addEventListener('click', restar2);
+
 /*LATA PALE ALE*/
 const nombreAle = document.getElementById("nombreAle");
 nombreAle.innerText = `${lataPaleAle.tipo} ${lataPaleAle.variedad}`;
 const precioAle = document.getElementById("precioAle");
 precioAle.innerText = `$${lataPaleAle.precio}`;
+const botonPaleAle = document.getElementById("botonPaleAle");
+/*SUMADOR DE CANTIDAD*/
+let total3 = document.getElementById('cantidad3');
+function sumar3 () {
+    let valorTotal3 = parseInt(total3.innerHTML);
+    total3.innerHTML = valorTotal3 + 1;
+}
+function restar3 () {
+    let valorTotal3 = parseInt(total3.innerHTML);
+    if (valorTotal3 == 1) return;
+    total3.innerHTML = valorTotal3 - 1;
+}
+document.getElementById('sumar3').addEventListener('click', sumar3);
+document.getElementById('restar3').addEventListener('click', restar3);
+
 /*LATA Dole Pale*/
 const nombreDoble = document.getElementById("nombreDoble");
 nombreDoble.innerText = `${lataDoblePale.tipo} ${lataDoblePale.variedad}`;
 const precioDoble = document.getElementById("precioDoble");
 precioDoble.innerText = `$${lataDoblePale.precio}`;
+const botonDoblePale = document.getElementById("botonDoblePale");
+/*SUMADOR DE CANTIDAD*/
+let total4 = document.getElementById('cantidad4');
+function sumar4 () {
+    let valorTotal4 = parseInt(total4.innerHTML);
+    total4.innerHTML = valorTotal4 + 1;
+}
+function restar4 () {
+    let valorTotal4 = parseInt(total4.innerHTML);
+    if (valorTotal4 == 1) return;
+    total4.innerHTML = valorTotal4 - 1;
+}
+document.getElementById('sumar4').addEventListener('click', sumar4);
+document.getElementById('restar4').addEventListener('click', restar4);
+
 /*LATA HIBISCUS*/
 const nombreHibiscus = document.getElementById("nombreHibiscus");
 nombreHibiscus.innerText = `${lataHibiscus.tipo} ${lataHibiscus.variedad}`;
 const precioHibiscus = document.getElementById("precioHibiscus");
 precioHibiscus.innerText = `$${lataHibiscus.precio}`;
+const botonHibiscus = document.getElementById("botonHibiscus");
+/*SUMADOR DE CANTIDAD*/
+let total5 = document.getElementById('cantidad5');
+function sumar5 () {
+    let valorTotal5 = parseInt(total5.innerHTML);
+    total5.innerHTML = valorTotal5 + 1;
+}
+function restar5 () {
+    let valorTotal5 = parseInt(total5.innerHTML);
+    if (valorTotal5 == 1) return;
+    total5.innerHTML = valorTotal5 - 1;
+}
+document.getElementById('sumar5').addEventListener('click', sumar5);
+document.getElementById('restar5').addEventListener('click', restar5);
+
 /*LATA FARAMIR*/
 const nombreFaramir = document.getElementById("nombreFaramir");
 nombreFaramir.innerText = `${lataFaramir.tipo} ${lataFaramir.variedad}`;
 const precioFaramir = document.getElementById("precioFaramir");
 precioFaramir.innerText = `$${lataFaramir.precio}`;
+const botonFaramir = document.getElementById("botonFaramir");
+/*SUMADOR DE CANTIDAD*/
+let total6 = document.getElementById('cantidad6');
+function sumar6 () {
+    let valorTotal6 = parseInt(total6.innerHTML);
+    total6.innerHTML = valorTotal6 + 1;
+}
+function restar6 () {
+    let valorTotal6 = parseInt(total6.innerHTML);
+    if (valorTotal6 == 1) return;
+    total6.innerHTML = valorTotal6 - 1;
+}
+document.getElementById('sumar6').addEventListener('click', sumar6);
+document.getElementById('restar6').addEventListener('click', restar6);
+
 /*GROWLER TRADICIONAL*/
 const nombreGrowlerTradicional = document.getElementById("nombreGrowlerTradicional");
 nombreGrowlerTradicional.innerText = `${growlerTradicional.tipo} ${growlerTradicional.variedad}`;
 const precioGrowlerTradicional = document.getElementById("precioGrowlerTradicional");
 precioGrowlerTradicional.innerText = `$${growlerTradicional.precio}`;
+const botonGrowlerTrad = document.getElementById("botonGrowlerTrad");
+/*SUMADOR DE CANTIDAD*/
+let total7 = document.getElementById('cantidad7');
+function sumar7 () {
+    let valorTotal7 = parseInt(total7.innerHTML);
+    total7.innerHTML = valorTotal7 + 1;
+}
+function restar7 () {
+    let valorTotal7 = parseInt(total7.innerHTML);
+    if (valorTotal7 == 1) return;
+    total7.innerHTML = valorTotal7 - 1;
+}
+document.getElementById('sumar7').addEventListener('click', sumar7);
+document.getElementById('restar7').addEventListener('click', restar7);
+
 /*GROWLER ESTACION*/
 const nombreGrowlerEstacion = document.getElementById("nombreGrowlerEstacion");
 nombreGrowlerEstacion.innerText = `${growlerEstacion.tipo} ${growlerEstacion.variedad}`;
 const precioGrowlerEstacion = document.getElementById("precioGrowlerEstacion");
 precioGrowlerEstacion.innerText = `$${growlerEstacion.precio}`;
+const botonGrowlerEst = document.getElementById("botonGrowlerEst");
+/*SUMADOR DE CANTIDAD*/
+let total8 = document.getElementById('cantidad8');
+function sumar8 () {
+    let valorTotal8 = parseInt(total8.innerHTML);
+    total8.innerHTML = valorTotal8 + 1;
+}
+function restar8 () {
+    let valorTotal8 = parseInt(total8.innerHTML);
+    if (valorTotal8 == 1) return;
+    total8.innerHTML = valorTotal8 - 1;
+}
+document.getElementById('sumar8').addEventListener('click', sumar8);
+document.getElementById('restar8').addEventListener('click', restar8);
+
 /*GROWLER AUTOR*/
 const nombreGrowlerAutor = document.getElementById("nombreGrowlerAutor");
 nombreGrowlerAutor.innerText = `${growlerAutor.tipo} ${growlerAutor.variedad}`;
 const precioGrowlerAutor = document.getElementById("precioGrowlerAutor");
 precioGrowlerAutor.innerText = `$${growlerAutor.precio}`;
+const botonGrowlerAut = document.getElementById("botonGrowlerAut");
+/*SUMADOR DE CANTIDAD*/
+let total9 = document.getElementById('cantidad9');
+function sumar9 () {
+    let valorTotal9 = parseInt(total9.innerHTML);
+    total9.innerHTML = valorTotal9 + 1;
+}
+function restar9 () {
+    let valorTotal9 = parseInt(total9.innerHTML);
+    if (valorTotal9 == 1) return;
+    total9.innerHTML = valorTotal9 - 1;
+}
+document.getElementById('sumar9').addEventListener('click', sumar9);
+document.getElementById('restar9').addEventListener('click', restar9);
+
 /*GROWLER ENVASE*/
 const nombreGrowlerEnvase = document.getElementById("nombreGrowlerEnvase");
 nombreGrowlerEnvase.innerText = `${growlerEnvase.tipo} ${growlerEnvase.variedad}`;
 const precioGrowlerEnvase = document.getElementById("precioGrowlerEnvase");
 precioGrowlerEnvase.innerText = `$${growlerEnvase.precio}`;
+const botonGrowlerEnvase = document.getElementById("botonGrowlerEnvase");
+/*SUMADOR DE CANTIDAD*/
+let total10 = document.getElementById('cantidad10');
+function sumar10 () {
+    let valorTotal10 = parseInt(total10.innerHTML);
+    total10.innerHTML = valorTotal10 + 1;
+}
+function restar10 () {
+    let valorTotal10 = parseInt(total10.innerHTML);
+    if (valorTotal10 == 1) return;
+    total10.innerHTML = valorTotal10 - 1;
+}
+document.getElementById('sumar10').addEventListener('click', sumar10);
+document.getElementById('restar10').addEventListener('click', restar10);
+
 /*SIX PACK HONEY*/
 const nombreSixTrad = document.getElementById("nombreSixTrad");
 nombreSixTrad.innerText = `${sixPackHoney.tipo} ${sixPackHoney.variedad}`;
 const precioSixTrad = document.getElementById("precioSixTrad");
 precioSixTrad.innerText = `$${sixPackHoney.precio}`;
+const botonSixPackTrad = document.getElementById("botonSixPackTrad");
+/*SUMADOR DE CANTIDAD*/
+let total11 = document.getElementById('cantidad11');
+function sumar11 () {
+    let valorTotal11 = parseInt(total11.innerHTML);
+    total11.innerHTML = valorTotal11 + 1;
+}
+function restar11 () {
+    let valorTotal11 = parseInt(total11.innerHTML);
+    if (valorTotal11 == 1) return;
+    total11.innerHTML = valorTotal11 - 1;
+}
+document.getElementById('sumar11').addEventListener('click', sumar11);
+document.getElementById('restar11').addEventListener('click', restar11);
+
 /*SIX PACK FARAMIR*/
 const nombreSixAutor = document.getElementById("nombreSixAutor");
 nombreSixAutor.innerText = `${sixPackHibiscus.tipo} ${sixPackHibiscus.variedad}`;
 const precioSixAutor = document.getElementById("precioSixAutor");
 precioSixAutor.innerText = `$${sixPackHibiscus.precio}`;
+const botonSixPackAut = document.getElementById("botonSixPackAut");
+/*SUMADOR DE CANTIDAD*/
+let total12 = document.getElementById('cantidad12');
+function sumar12 () {
+    let valorTotal12 = parseInt(total12.innerHTML);
+    total12.innerHTML = valorTotal12 + 1;
+}
+function restar12 () {
+    let valorTotal12 = parseInt(total12.innerHTML);
+    if (valorTotal12 == 1) return;
+    total12.innerHTML = valorTotal12 - 1;
+}
+document.getElementById('sumar12').addEventListener('click', sumar12);
+document.getElementById('restar12').addEventListener('click', restar12);
+
 /*12 PACK HONEY*/
 const nombre12Trad = document.getElementById("nombre12Trad");
 nombre12Trad.innerText = `${twelvePackHoney.tipo} ${twelvePackHoney.variedad}`;
 const precio12Trad = document.getElementById("precio12Trad");
 precio12Trad.innerText = `$${twelvePackHoney.precio}`;
+const boton12PackTrad = document.getElementById("boton12PackTrad");
+/*SUMADOR DE CANTIDAD*/
+let total13 = document.getElementById('cantidad13');
+function sumar13 () {
+    let valorTotal13 = parseInt(total13.innerHTML);
+    total13.innerHTML = valorTotal13 + 1;
+}
+function restar13 () {
+    let valorTotal13 = parseInt(total13.innerHTML);
+    if (valorTotal13 == 1) return;
+    total13.innerHTML = valorTotal13 - 1;
+}
+document.getElementById('sumar13').addEventListener('click', sumar13);
+document.getElementById('restar13').addEventListener('click', restar13);
+
 /*12 PACK FARAMIR*/
 const nombre12Autor = document.getElementById("nombre12Autor");
 nombre12Autor.innerText = `${twelvePackHibiscus.tipo} ${twelvePackHibiscus.variedad}`;
 const precio12Autor = document.getElementById("precio12Autor");
 precio12Autor.innerText = `$${twelvePackHibiscus.precio}`;
+const boton12PackAut = document.getElementById("boton12PackAut");
+/*SUMADOR DE CANTIDAD*/
+let total14 = document.getElementById('cantidad14');
+function sumar14 () {
+    let valorTotal14 = parseInt(total14.innerHTML);
+    total14.innerHTML = valorTotal14 + 1;
+}
+function restar14 () {
+    let valorTotal14 = parseInt(total14.innerHTML);
+    if (valorTotal14 == 1) return;
+    total14.innerHTML = valorTotal14 - 1;
+}
+document.getElementById('sumar14').addEventListener('click', sumar14);
+document.getElementById('restar14').addEventListener('click', restar14);
+
 /*24 PACK HONEY*/
 const nombre24Trad = document.getElementById("nombre24Trad");
 nombre24Trad.innerText = `${twentyfourPackHoney.tipo} ${twentyfourPackHoney.variedad}`;
 const precio24Trad = document.getElementById("precio24Trad");
 precio24Trad.innerText = `$${twentyfourPackHoney.precio}`;
+const boton24PackTrad = document.getElementById("boton24PackTrad");
+/*SUMADOR DE CANTIDAD*/
+let total15 = document.getElementById('cantidad15');
+function sumar15 () {
+    let valorTotal15 = parseInt(total15.innerHTML);
+    total15.innerHTML = valorTotal15 + 1;
+}
+function restar15 () {
+    let valorTotal15 = parseInt(total15.innerHTML);
+    if (valorTotal15 == 1) return;
+    total15.innerHTML = valorTotal15 - 1;
+}
+document.getElementById('sumar15').addEventListener('click', sumar15);
+document.getElementById('restar15').addEventListener('click', restar15);
+
 /*24 PACK FARAMIR*/
 const nombre24Autor = document.getElementById("nombre24Autor");
 nombre24Autor.innerText = `${twentyfourPackHibiscus.tipo} ${twentyfourPackHibiscus.variedad}`;
 const precio24Autor = document.getElementById("precio24Autor");
 precio24Autor.innerText = `$${twentyfourPackHibiscus.precio}`;
+const boton24PackAut = document.getElementById("boton24PackAut");
+/*SUMADOR DE CANTIDAD*/
+let total16 = document.getElementById('cantidad16');
+function sumar16 () {
+    let valorTotal16 = parseInt(total16.innerHTML);
+    total16.innerHTML = valorTotal16 + 1;
+}
+function restar16 () {
+    let valorTotal16 = parseInt(total16.innerHTML);
+    if (valorTotal16 == 1) return;
+    total16.innerHTML = valorTotal16 - 1;
+}
+document.getElementById('sumar16').addEventListener('click', sumar16);
+document.getElementById('restar16').addEventListener('click', restar16);
+
 /*GORRA AZUL*/
 const nombreGorraAzul = document.getElementById("nombreGorraAzul");
 nombreGorraAzul.innerText = `${gorraAzul.tipo} ${gorraAzul.variedad}`;
 const precioGorraAzul = document.getElementById("precioGorraAzul");
 precioGorraAzul.innerText = `$${gorraAzul.precio}`;
+const botonGorraAzul = document.getElementById("botonGorraAzul");
+/*SUMADOR DE CANTIDAD*/
+let total17 = document.getElementById('cantidad17');
+function sumar17 () {
+    let valorTotal17 = parseInt(total17.innerHTML);
+    total17.innerHTML = valorTotal17 + 1;
+}
+function restar17 () {
+    let valorTotal17 = parseInt(total17.innerHTML);
+    if (valorTotal17 == 1) return;
+    total17.innerHTML = valorTotal17 - 1;
+}
+document.getElementById('sumar17').addEventListener('click', sumar17);
+document.getElementById('restar17').addEventListener('click', restar17);
+
 /*GORRA GRIS*/
 const nombreGorraGris = document.getElementById("nombreGorraGris");
 nombreGorraGris.innerText = `${gorraGris.tipo} ${gorraGris.variedad}`;
 const precioGorraGris = document.getElementById("precioGorraGris");
 precioGorraGris.innerText = `$${gorraGris.precio}`;
+const botonGorraGris = document.getElementById("botonGorraGris");
+/*SUMADOR DE CANTIDAD*/
+let total18 = document.getElementById('cantidad18');
+function sumar18 () {
+    let valorTotal18 = parseInt(total18.innerHTML);
+    total18.innerHTML = valorTotal18 + 1;
+}
+function restar18 () {
+    let valorTotal18 = parseInt(total18.innerHTML);
+    if (valorTotal18 == 1) return;
+    total18.innerHTML = valorTotal18 - 1;
+}
+document.getElementById('sumar18').addEventListener('click', sumar18);
+document.getElementById('restar18').addEventListener('click', restar18);
+
 /*COPA*/
 const nombreCopa = document.getElementById("nombreCopa");
 nombreCopa.innerText = `${copa.tipo} ${copa.variedad}`;
 const precioCopa = document.getElementById("precioCopa");
 precioCopa.innerText = `$${copa.precio}`;
+const botonCopa = document.getElementById("botonCopa");
+/*SUMADOR DE CANTIDAD*/
+let total19 = document.getElementById('cantidad19');
+function sumar19 () {
+    let valorTotal19 = parseInt(total19.innerHTML);
+    total19.innerHTML = valorTotal19 + 1;
+}
+function restar19 () {
+    let valorTotal19 = parseInt(total19.innerHTML);
+    if (valorTotal19 == 1) return;
+    total19.innerHTML = valorTotal19 - 1;
+}
+document.getElementById('sumar19').addEventListener('click', sumar19);
+document.getElementById('restar19').addEventListener('click', restar19);
+
 
 // /*MENU*/
 // function menu(){
@@ -460,4 +755,44 @@ precioCopa.innerText = `$${copa.precio}`;
 //                     <button> Agregar al Carrito </button>`;
 //     contenedorProductos.appendChild(div);
 // })
+
+// //EVENTOS
+// //Son la manera que tenemos en JS de contraolara las acciones de los usuarios
+// //Se puede hacer que un mismo click haga dos comportamentos diferentes
+
+// //1) MEDIANTE ADD EVENT LISTENER
+// const btn = document.getElementById("btn");
+// btn.addEventListener("click", () => {
+//     console.log ("hola, me hiciste click")
+// });
+// //El EVENTO CLICK DISPARA AL CLICKEAR CON EL MOUSE
+// //Usamos en este caso una funcion flecha para asosiar un comportamiento a este evento
+
+// //2) UTILIZANDO PROPIEDADES DEL NODO
+// btn.onclick = () => {
+//     alert("Segunda FOrma")
+// };
+
+// //3) FUNCION EN EL HTML
+// //Por ej 
+// onclick="JSenHTML()"; //en la etiqueta HTML
+// function JSenHTML() { //Esto linkea con js
+//     console.log("HOla")
+// };
+
+// //EVENTOS MAS COMUNES
+// //RELACIONADOS CON EL MOUSE
+// //Se producen con la ineraccion del usuario con el mouse
+// //MOUS UP O DOWN cuando se oprime o suelta el boton del raton
+// btn.onmousedown = () => {
+//     console.log("Oprimiste el boton")
+// };
+// //MOUEOVER MOUSEOUT El puntero sale o se mueve sobre el elemento
+// btn.onmouseover = () => { //cuando entrea al elemento el mouse
+//     console.log("INgreso el puntero")
+// };
+// //MOUSEMOVE
+// btn.onmousemove = () => { //mover el mouse sobre el elemento
+//     console.log("Pasaste el Mouse or a caja")
+// }
 
