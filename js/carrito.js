87af84f976677c28c154965cc154b32d3e2a6136
Manu Sucaria <manusucaria@gmail.com>
2022-10-04 09:15:23 -0300

    4 del 10 1

--- js/carrito.js
+++ js/carrito.js
@@ -86,10 +86,10 @@ function vaciar () {
     location.reload()
 };
 /*COLOR CARRITO*/
-let svg = document.getElementById("svg-carrito");
-if (pedido = localStorage.getItem("pedido")){
-    svg.classList.add = "svg-cargado";
-    console.log(svg)
-}
+// let svg = document.getElementById("svg-carrito");
+// if (pedido = localStorage.getItem("pedido")){
+//     svg.classList.add = "svg-cargado";
+//     console.log(svg)
+// }
 /*CONTINUAR COMPRA*/
 
